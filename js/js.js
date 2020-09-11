856610cb5b833f7ca76c2e23f2774744862c2fb1
haibin-gua <1305263409@qq.com>
2020-09-12 04:10:39 +0800

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,34 @@
+// $(document).ready(function(){
+//     data = $.ajax({url:'http:localhost:3000/api/bbs/test'});
+//     console.log(data)
+// })
+
+// $().ready(function(){
+//     //在键盘按下并释放及提交后验证提交表单
+//     $('#signinForm').vaildate({
+//         rules:{
+//             username:{
+//                 required:true,
+//                 minlength:6,
+//                 maxlength:12
+//             },
+//             pwd:{
+//                 required:true,
+//                 minlength:6,
+//                 maxlength:12
+//             }
+//         },
+//         message:{
+//             username:{
+//                 required:'请输入用户名',
+//                 minlength:'用户名长度不能小于6个字符',
+//                 maxlength:'用户名长度不能大于12个字符'
+//             },
+//             pwd:{
+//                 required:'请输入密码',
+//                 minlength:'密码长度不能小于6个字符',
+//                 maxlength:'密码长度不能大于12个字符'
+//             }
+//         }
+//     });
+// });
